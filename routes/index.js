a6e84ed1a6db0162d6b7fc956b3e7762b6e29868
ifotn <rich@infrontofthenet.com>
2016-03-04 10:51:48 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,108 @@
+var express = require('express');
+var router = express.Router();
+
+// needed to parse contact form body request
+var bodyParser = require('body-parser');
+
+// used for url request to google's recaptcha api
+var https = require('https');
+
+// used for contact form email
+var nodemailer = require('nodemailer');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Rich Freeman Portfolio' });
+});
+
+/* GET about */
+router.get('/about', function(req, res, next) {
+    res.render('about', { title: 'About'} );
+});
+
+/* GET projects */
+router.get('/projects', function(req, res, next) {
+    res.render('projects', { title: 'Projects'} );
+});
+
+/* GET services */
+router.get('/services', function(req, res, next) {
+    res.render('services', { title: 'Services'} );
+});
+
+/* GET contact */
+router.get('/contact', function(req, res, next) {
+    res.render('contact', { title: 'Contact'} );
+});
+
+/* POST contact */
+router.post('/contact', function(req, res, next) {
+    
+    // check recaptcha first
+    verifyRecaptcha(req.body['g-recaptcha-response'], function(success) {
+        if (success) {
+            
+            // send email
+            var message = '<h4>Portfolio Contact Request<h4><p>Name: ' + req.body.name + '</p><p>Email: ' + req.body.email + '</p><p>Phone: ' + req.body.phone + '</p><p>Message: ' + req.body.message + '</p>';
+            
+            var mailOptions = {
+                from: 'gcevstation@gmail.com',
+                to: 'rich@infrontofthenet.com',
+                subject: 'Portfolio Contact Request',
+                html: message
+            }
+            
+            transporter.sendMail(mailOptions, function(err, info) {
+                if (err) {
+                    res.end('Could not send ' + err.toString());
+                }
+                else {
+                    // show thank you
+                    res.render('thankyou', { title: 'Thanks!}' });
+                }
+            });
+                  
+        }
+        else {
+            res.end('You don\'t look human to me.  Try again.');
+        }
+    });
+    
+
+});
+
+// recaptcha secret key
+var SECRET = '6Le7mw0TAAAAAK8HlzXnh_sOE_5jqSuAuqWZ3duk';
+
+// helper function to call recaptcha API and parse response from Google - courtesy Jonathan Warner @jaxbot
+function verifyRecaptcha(key, callback) {
+    https.get('https://www.google.com/recaptcha/api/siteverify?secret=' + SECRET + '&response=' + key, function(res) {
+        
+        var data = '';
+        
+        res.on('data', function(chunk) {
+            data += chunk.toString();
+        });
+        
+        res.on('end', function() {
+           try {
+               var parsedData = JSON.parse(data);
+               callback(parsedData.success);
+           } 
+            catch(e) {
+                callback(false);
+            }
+        });
+    });
+}
+
+// mail transporter
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'gcevstation@gmail.com',
+        pass: '8x8F?C54'
+    }
+});
+
+module.exports = router;
